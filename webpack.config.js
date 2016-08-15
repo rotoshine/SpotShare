b680df5af3fc80792937f133dd3ea92ec26905d1
김태희(로토) <rotoshine@coupang.com>
2016-08-15 23:55:00 +0900

    댓글기능 추가

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,21 @@ require('dotenv').config();
 var path = require('path');
 var webpack = require('webpack');
 
-module.exports = {
-  devtool: 'eval',
-  entry: [
+var entry = [
+  './client/js/src/index'
+];
+
+if(process.env !== 'production'){
+  entry = [
     'webpack/hot/dev-server',
     'webpack-hot-middleware/client',
     './client/js/src/index'
-  ],
+  ];
+}
+
+module.exports = {
+  devtool: 'eval',
+  entry: entry,
   output: {
     path: path.join(__dirname, 'client/js/dist'),
     filename: 'bundle.js',
