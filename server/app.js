fad45ec95f3d848e00aafa468abc83a53d22cc34
김태희(로토) <rotoshine@coupang.com>
2016-12-05 18:19:59 +0900

    test

--- server/app.js
+++ server/app.js
@@ -22,6 +22,7 @@ const webpackConfig = require('../webpack.config.js');
 const parseArgs = require('minimist');
 
 console.log('loaded config', config);
+console.log(process.env.NODE_ENV);
 const isDevMode = process.env.NODE_ENV !== 'production';
 
 // webpack setting
